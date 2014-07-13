6a71dab76ea4644592a0974fe99dbb717cca0244
Hector Ghinaglia <hghinaglia@gmail.com>
2014-07-13 11:12:22 -0430

    Archivo de constantes de direcciones
    * Agregadas direcciones de toda la web, sugerencia: eliminar las no
    necesarias.

---
+++ js/const.js
@@ -0,0 +1,20 @@
+
+
+const SERVER            = "http://localhost"
+//const SERVER          = "http://www.memetomia.com"
+
+const RESOURCES         = "/resources"
+const FONTS             = "/resources/fonts"
+const MEDIA             = "/media"
+const JS                = "/js"
+const MODAL_CONTROLLERS = "/js/modal_controllers"
+const CSS               = "/css"
+const BASE              = "/base"
+const AJAX              = "/ajax"
+
+const ADMIN             = "/admin"
+const ADMIN_CSS         = "/admin/css"
+const ADMIN_JS          = "/admin/js"
+
+
+
