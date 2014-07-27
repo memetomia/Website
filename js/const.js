4178df04ce1ea8c9f6d18bf98218d8db9c749463
jaivic <jaivicx10a@gmail.com>
2014-07-27 09:09:12 -0700

    ultimo archivo
    archivo para subir imagenes, pronto subo mas contenido hoy full
    programacion

--- js/const.js
+++ js/const.js
@@ -1,20 +1,18 @@
-
-
-const SERVER            = "http://localhost"
-//const SERVER          = "http://www.memetomia.com"
-
-const RESOURCES         = "/resources"
-const FONTS             = "/resources/fonts"
-const MEDIA             = "/media"
-const JS                = "/js"
-const MODAL_CONTROLLERS = "/js/modal_controllers"
-const CSS               = "/css"
-const BASE              = "/base"
-const AJAX              = "/ajax"
-
-const ADMIN             = "/admin"
-const ADMIN_CSS         = "/admin/css"
-const ADMIN_JS          = "/admin/js"
+var SERVER  = "http://localhost:8080/memetomia";
+//var SERVER          = "http://www.memetomia.com"
+var RESOURCES         = "/resources";
+var FONTS             = "/resources/fonts";
+var MEDIA             = "/media";
+var MEME             = SERVER+MEDIA+"/meme";
+var ARTICLE             = SERVER+MEDIA+"/article";
+var JS                = "/js";
+var MODAL_CONTROLLERS = "/js/modal_controllers";
+var CSS               = "/css";
+var BASE              = "/base";
+var AJAX              = "/ajax";
+var ADMIN             = "/admin";
+var ADMIN_CSS         = "/admin/css";
+var ADMIN_JS          = "/admin/js";
 
 
 
