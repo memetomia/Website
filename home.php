05c72228ad2673f94521e0ce987c25f7a548ae6a
jaivic <jaivicx10a@gmail.com>
2014-08-06 08:23:51 -0700

    ultimo cambios
    cambie el time line y el home y subi un arhivo de imagenes

--- home.php
+++ home.php
@@ -37,18 +37,12 @@
                     </button>
                     <a class="navbar-brand" href="#">Memetomía</a>
                 </div>
-<<<<<<< HEAD:home.php
-<<<<<<< HEAD:home.php
-                
                 <div class="collapse navbar-collapse" id="bs-media/example-navbar-collapse-1">
-=======
+
+
+           
 
                 <div class="collapse navbar-collapse" id="memetomia-navbar">
->>>>>>> origin/master:not-logged.php
-=======
-
-                <div class="collapse navbar-collapse" id="memetomia-navbar">
->>>>>>> origin/master:not-logged.php
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="#">Inicio</a></li>
                         <li><a href="#">Top</a></li>
@@ -142,15 +136,12 @@
 
         <!--Ver donde poner esto-->
         <script>
-<<<<<<< HEAD:home.php
             // Activating All Switches
             $(".settings-switch").bootstrapSwitch();
-=======
             // activa modal registro al presionar botones
             $('.comment-button, .like-button').click(function() {
                 $('#sign-in-modal').modal('show');
             });
->>>>>>> origin/master:not-logged.php
         </script>
     </body>
 </html>
