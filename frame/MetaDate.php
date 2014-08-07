85cf7d27979e2edd687397332a339a9dfc1e6a35
jaivic <jaivicx10a@gmail.com>
2014-08-07 10:17:11 -0700

    desarrollando el memetomiazador y otras cosas mas
    inicio el memetomiazador del admin
    y ademas creo unas metadatas para la pagina de articulo individual
    nota se arreglaron query de la tabla gallery

---
+++ frame/MetaDate.php
@@ -0,0 +1,15 @@
+
+   <meta name="og:locale" value="es_ES"><meta property="og:locale" content="es_ES">
+            <meta name="og:site_name" value="Memetomia"><meta property="og:site_name" content="Memetomia">
+            <meta name="twitter:card" value="photo"><meta property="twitter:card" content="photo">
+            <meta name="twitter:site" value="@memetomia"><meta property="twitter:site" content="@memetomia">
+            <meta name="twitter:creator" value="@memetomia"><meta property="twitter:creator" content="@memetomia">
+            <meta name="twitter:title" value="@memetomia"><meta property="twitter:title" content="@memetomia">
+            <meta name="twitter:domain" value="memetomia.com"><meta property="twitter:domain" content="http://memetomia.com/">
+            <meta name="twitter:description" value="Paginas de imagenes y videos graciosos todos en un mismo luga"><meta property="twitter:description" content="Paginas de imagenes y videos graciosos todos en un mismo lugar">
+            <meta name="title" value=""><meta property="title" content="">
+            <meta name="og:description" value="Todas la imágenes graciosas de la red en un solo lugar!!!"><meta property="og:description" content="Todas la imágenes graciosas de la red en un solo lugar!!!">
+            <meta name="og:title" value=""><meta property="og:title" content="">
+            <meta name="twitter:title" value=""><meta property="twitter:title" content="">
+            <meta name="twitter:image:src" value="http://memetomia.com/http://img.youtube.com/vi/44XYEeD1A1U/0.jpg"><meta property="twitter:image:src" content="http://memetomia.com/http://img.youtube.com/vi/44XYEeD1A1U/0.jpg">
+            <meta name="og:image" value="http://memetomia.com/http://img.youtube.com/vi/44XYEeD1A1U/0.jpg"><meta property="og:image" content="http://memetomia.com/http://img.youtube.com/vi/44XYEeD1A1U/0.jpg">
