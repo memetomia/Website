1dbb664e2090c47a80d9df89e861610bb7a5f496
hegd26 <hghinaglia@gmail.com>
2014-08-07 15:58:24 -0430

    Maquetadas paginas image y image-not-logged
    Estructura de páginas, lista, agregado footer básico (falta configurar), modificado elementos css dentro de las páginas image y image-not-logged

--- image-not-logged.php
+++ image-not-logged.php
@@ -62,11 +62,6 @@
                     </button>
                     <a class="navbar-brand" href="#">Memetomía</a>
                 </div>
-                <div class="collapse navbar-collapse" id="bs-media/example-navbar-collapse-1">
-
-
-           
-
                 <div class="collapse navbar-collapse" id="memetomia-navbar">
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="#">Inicio</a></li>
@@ -75,28 +70,11 @@
                         <li><a href="#">GIF</a></li>
                     </ul>                    
                     <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-bell"></span> Notificaciones <span class="label label-danger">2</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#"><span class="not-readed"></span> <span class="glyphicon glyphicon-comment"></span> Alejandro123 comentó en tu post</a></li>                                
-                                <li><a href="#"><span class="not-readed"></span> <span class="glyphicon glyphicon-thumbs-up"></span> A Erika33 le gustó tu post</a></li>                                
-                                <li class="divider"></li>
-                                <li class="text-center"><a href="#" data-toggle="modal" data-target="#notifications-modal">Ver todas las notificaciones</a></li>                                
-                            </ul>
-                        </li>
-                        <li><a href="#" data-toggle="modal" data-target="#new-post-modal"><span class="glyphicon glyphicon-upload"></span> Nuevo Post</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jaivic Villegas<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#" data-toggle="modal" data-target="#activity-modal"><span class="glyphicon glyphicon-tasks"></span> Actividad</a></li>                                
-                                <li><a href="#" data-toggle="modal" data-target="#settings-modal"><span class="glyphicon glyphicon-cog"></span> Ajustes</a></li>                                
-                                <li class="divider"></li>
-                                <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesión</a></li>
-                            </ul>
-                        </li>
+                        <li><a href="#" data-toggle="modal" data-target="#sign-in-modal">¡Regístrate en Memetomía!</a></li>
+                        <li><a href="#" data-toggle="modal" data-target="#login-modal">Iniciar sesión</a></li>                        
                     </ul>                    
+
                 </div>
-            </div>
         </nav>
         
         <div id="main-content" class="container">            
@@ -124,7 +102,7 @@
                 <!-- BUTTONS -->
                 <div class="post-options row">
                     <div class="col-md-4">
-                        <button type="button" class="btn btn-default btn-block" data-toggle="button">
+                        <button type="button" class="btn btn-default btn-block like-button">
                             <span class="glyphicon glyphicon-thumbs-up"></span> Me gusta
                         </button>
                     </div>
@@ -255,20 +233,19 @@
         
 
         <!-- MODAL Windows -->
-        <?php             
-        include_once 'modal/notifications-modal.php';
-        include_once 'modal/new-post-modal.php'; 
-        include_once 'modal/activity-modal.php';  
-        include_once 'modal/settings-modal.php';                    
+        <?php
+        include_once 'modal/login-modal.php';
+        include_once 'modal/sign-in-modal.php';
         ?>        
-        
-      
-      
+
 
         <!--Ver donde poner esto-->
         <script>
-            // Activating All Switches
-            $(".settings-switch").bootstrapSwitch();            
+            // activa modal registro al presionar botones
+            $('.like-button, #report-container a, #comment-button-container button').click(function(event) {
+                event.preventDefault()
+                $('#sign-in-modal').modal('show')
+            });
         </script>
     </body>
 </html>
