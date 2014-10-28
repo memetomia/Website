563a21582ce27f370c6f84542a2ece1fb17c2e8b
jaivic <jaivicx10a@gmail.com>
2014-10-27 21:53:34 -0500

    notificacion lista y numero de notificaciones
    se agrego las notificaicones el menu y el numero de notificaciones por
    ajax

--- frame/bar.php
+++ frame/bar.php
@@ -1,59 +1,55 @@
 <?php
+if ($bTodoSimple != true) {
+    if ($co->IsSession()) {
 
-if ($bTodoSimple!=true){
-if ($co->IsSession()) {
-   
 //   $bdUser->SearchByID($co->getSVar("iId"));
 //   $user=$bdUser->bd->obtener_respuesta(0,"NAME");
-?>
-<nav class="navbar navbar-static-top navbar-inverse" role="navigation">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#memetomia-navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="<?php echo SERVER; ?>">Memetomía</a>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-media/example-navbar-collapse-1">
-            <div class="collapse navbar-collapse" id="memetomia-navbar">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="<?php echo SERVER; ?>">Inicio</a></li>
-                    <li><a href="#">Top</a></li>
+        ?>
+        <nav class="navbar navbar-static-top navbar-inverse" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#memetomia-navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="<?php echo SERVER; ?>">Memetomía</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-media/example-navbar-collapse-1">
+                    <div class="collapse navbar-collapse" id="memetomia-navbar">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="<?php echo SERVER; ?>">Inicio</a></li>
+                            <li><a href="#">Top</a></li>
 
-                </ul>                    
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-bell"></span> Notificaciones <span class="label label-danger">2</span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><span class="not-readed"></span> <span class="glyphicon glyphicon-comment"></span> Alejandro123 comentó en tu post</a></li>                                
-                            <li><a href="#"><span class="not-readed"></span> <span class="glyphicon glyphicon-thumbs-up"></span> A Erika33 le gustó tu post</a></li>                                
-                            <li class="divider"></li>
-                            <li class="text-center"><a href="#" data-toggle="modal" data-target="#notifications-modal">Ver todas las notificaciones</a></li>                                
-                        </ul>
-                    </li>
-                    <li><a href="#" data-toggle="modal" data-target="#new-post-modal"><span class="glyphicon glyphicon-upload"></span> Nuevo Post</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php ECHO $user; ?><b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#" data-toggle="modal" data-target="#activity-modal"><span class="glyphicon glyphicon-tasks"></span> Actividad</a></li>                                
-                            <li><a href="#" data-toggle="modal" data-target="#settings-modal"><span class="glyphicon glyphicon-cog"></span> Ajustes</a></li>                                
-                            <li class="divider"></li>
-                            <li><a href="LogOut.php"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesión</a></li>
-                        </ul>
-                    </li>
-                </ul>                    
-            </div>
-        </div>
-</nav>
-<?php
-}else{
-    
-
-?>    
-    <nav class="navbar navbar-static-top navbar-inverse" role="navigation">
+                        </ul>                    
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="dropdown">
+                                <a id="spanTitleNotify" href="#" class="dropdown-toggle" data-toggle="dropdown"></a>
+                                <ul  class="dropdown-menu">
+                                    <div  id="MenuDropNotify"></div>
+                                    <li class="divider"></li>
+                                    <li class="text-center"><a href="#" data-toggle="modal" data-target="#notifications-modal">Ver todas las notificaciones</a></li>                                
+                                </ul>
+                            </li>
+                            <li><a href="#" data-toggle="modal" data-target="#new-post-modal"><span class="glyphicon glyphicon-upload"></span> Nuevo Post</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php ECHO $user; ?><b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#" data-toggle="modal" data-target="#activity-modal"><span class="glyphicon glyphicon-tasks"></span> Actividad</a></li>                                
+                                    <li><a href="#" data-toggle="modal" data-target="#settings-modal"><span class="glyphicon glyphicon-cog"></span> Ajustes</a></li>                                
+                                    <li class="divider"></li>
+                                    <li><a href="LogOut.php"><span class="glyphicon glyphicon-log-out"></span> Cerrar sesión</a></li>
+                                </ul>
+                            </li>
+                        </ul>                    
+                    </div>
+                </div>
+        </nav>
+        <?php
+    } else {
+        ?>    
+        <nav class="navbar navbar-static-top navbar-inverse" role="navigation">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#memetomia-navbar">
@@ -75,29 +71,28 @@ if ($co->IsSession()) {
 
                 </div>
         </nav>
-<?php }
-}else{
-    
+    <?php
+    }
+} else {
+    ?>
+    <nav class="navbar navbar-static-top navbar-inverse" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#memetomia-navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo SERVER; ?>">Memetomía</a>
+            </div>
+            <div class="collapse navbar-collapse" id="memetomia-navbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="<?php echo SERVER; ?>">Inicio</a></li>
+                </ul>                    
+                <ul class="nav navbar-nav navbar-right">
+                </ul>                    
 
-?>
- <nav class="navbar navbar-static-top navbar-inverse" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#memetomia-navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="<?php echo SERVER; ?>">Memetomía</a>
-                </div>
-                <div class="collapse navbar-collapse" id="memetomia-navbar">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="<?php echo SERVER; ?>">Inicio</a></li>
-                    </ul>                    
-                    <ul class="nav navbar-nav navbar-right">
-                         </ul>                    
-
-                </div>
-        </nav>
+            </div>
+    </nav>
 <?php } ?>
--- modal/notifications-modal.php
+++ modal/notifications-modal.php
@@ -58,7 +58,9 @@
         var $_modal;
         var $_ulComentarios;
         var $_ulLike;
+        var $_ulMenuDrop;
         var $_markAsReadButton;
+        var $_iNNotify;
         /**
          * 
          *     inicia todo el modal carga los div y li necesarios
@@ -66,18 +68,21 @@
          *
          * 
          * @return 
-         *
+         *  
+         <li><a href="#"><span class="not-readed"></span> <span class="glyphicon glyphicon-thumbs-up"></span> A Erika33 le gustó tu post</a></li>                                
+         
          */
         function Iniciar() {
             MODAL_PREFIX = 'notifications-';
             CONST_NOT_READ = '<span class="not-readed"></span>';
             CONST_ICON_GLY = '<span class="glyphicon glyphicon-comment"></span>';
-
+            $_ulMenuDrop = $("#MenuDropNotify");
             $_modal = $('#' + MODAL_PREFIX + 'modal');
             $_ulComentarios = $('#' + MODAL_PREFIX + 'ul-comment');
             $_ulLike = $('#' + MODAL_PREFIX + 'ul-like');
             $_markAsReadButton = $('#' + MODAL_PREFIX + 'mark-as-read');
             $_markAsReadButton.click(NotReaded);
+            $_iNNotify = 0;
         }
 
         /**
@@ -92,18 +97,25 @@
                     $_ulComentarios.html("");
                     $_ulComentarios.append('<p class="text-muted">Comentarios</p>');
                     var comment = "";
+                    var notify = "";
                     var i;
+                    $_iNNotify = o.Tupla;
                     for (i = 0; i < o.Tupla; i++) {
                         if (o.aComment[i]["bSeeByOwner"] == 0) {
                             comment = CONST_NOT_READ + "   " + CONST_ICON_GLY;
+
                         } else {
                             comment = "   " + CONST_ICON_GLY;
                         }
                         comment += ' <a href="' + o.aComment[i]["iIDUser"] + '">' + o.aComment[i]["sNameUser"] + '</a>'
                         comment += " hizo un comentario en tu post: ";
                         comment += ' <a href="' + o.aComment[i]["iIDGalery"] + '">' + o.aComment[i]["sTitleImagen"] + '</a>'
+                        if ((i < 3) && (o.aComment[i]["bSeeByOwner"] == 0)) {
+                            notify += '<li><a href="' + o.aComment[i]["iIDGalery"] + '">' + CONST_NOT_READ + "   " + CONST_ICON_GLY + " " + o.aComment[i]["sNameUser"] + ' comentó en tu post</a></li>                                '
+                        }
                         $_ulComentarios.append('<li id="notifications-li-comment" class="list-group-item">' + comment + '</li>');
                     }
+                    $_ulMenuDrop.append(notify);
 
 
                 }
@@ -113,22 +125,39 @@
                     $_ulLike.html("");
                     $_ulLike.append('<p class="text-muted">Me Gusta</p>');
                     var comment = "";
+                    var notify = "";
                     var i;
+                    $_iNNotify = $_iNNotify + o.Tupla;
                     for (i = 0; i < o.Tupla; i++) {
                         if (o.aComment[i]["bSeeByOwner"] == 0) {
                             comment = CONST_NOT_READ + "   " + CONST_ICON_GLY;
                         } else {
                             comment = "   " + CONST_ICON_GLY;
                         }
-                        comment += ' <a href="' + o.aComment[i]["iIDUser"] + '">' + o.aComment[i]["sNameUser"] + '</a>'
+                        comment += ' <a href="' + o.aComment[i]["iIDUser"] + '">' + o.aComment[i]["sNameUser"] + '</a>';
                         comment += " le gustó tu post: ";
-                        comment += ' <a href="' + o.aComment[i]["iIDGalery"] + '">' + o.aComment[i]["sTitleImagen"] + '</a>'
+                        comment += ' <a href="' + o.aComment[i]["iIDGalery"] + '">' + o.aComment[i]["sTitleImagen"] + '</a>';
+                        if ((i < 3) && (o.aComment[i]["bSeeByOwner"] == 0)) {
+                            notify += '<li><a href="' + o.aComment[i]["iIDGalery"] + '">' + CONST_NOT_READ + "   " + CONST_ICON_GLY + " A " + o.aComment[i]["sNameUser"] + ' le gustó tu post</a></li>                                '
+                        }
                         $_ulLike.append('<li id="notifications-li-comment" class="list-group-item">' + comment + '</li>');
+                        $_ulMenuDrop.append(notify);
+                        showNumberNotify();
                     }
                 }
             }, "json");
         }/* fin de funcion Guardar*/
+        function showNumberNotify() {
+            if ($_iNNotify > 0) {
+                $("#spanTitleNotify").html('<span class = "glyphicon glyphicon-bell"> </span> Notificaciones <span  class="label label-danger">' + $_iNNotify + '</span >');
 
+            } else {
+               $("#spanTitleNotify").html('<span class = "glyphicon glyphicon-bell"> </span> Notificaciones <span  class="label label-danger">' + $_iNNotify + '</span >');
+
+
+            }
+
+        }
         /**
          * Evento que marca como leído todas las 
          * notificaciones pendientes
@@ -148,7 +177,8 @@
         return {
             Iniciar: Iniciar,
             Cargar: Cargar,
-            NotReaded: NotReaded
+            NotReaded: NotReaded,
+            showNumberNotify: showNumberNotify
 
 
         }
@@ -158,7 +188,7 @@
     {
         modalNotifications.Iniciar();
         modalNotifications.Cargar();
-
+      
 
     });
 </script>
